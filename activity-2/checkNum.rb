c9703fb8d0fcdfa1ddad4b227724438f1144b196
Jackie <jackiesalasdev@gmail.com>
2021-09-28 19:15:24 +0800

    finished act 2

--- activity-2/checkNum.rb
+++ activity-2/checkNum.rb
@@ -1,10 +1,46 @@
-a = [1,3,5,7,9,11]
-number = 3 
+#-------------------
+arr = [1, 3, 5, 7, 9, 11]
+num = 3
+if arr.each { |number| number == num }
+    puts "#{num} is found"
+end
 
-exist = a.select{|e| e == 3}
-if exist = true do
-  puts "checked"
-end 
-else puts "none"
 
+puts ""
+#---------------------------
+puts "please enter a number 1-100"
+numbers = gets.chop.to_i
+
+if numbers <= 50 
+    puts " The number scope of #{numbers} is 0-50"
+
+elsif numbers > 50 && numbers == 100
+    puts "The number scope of #{numbers} is 51-100"
+
+else 
+   puts "The number scope of #{numbers} is above 100"
+end
+
+
+
+
+#-------
+while true
+    puts "   Enter you name"
+    name = gets.chomp
+    
+    puts ""
+    stopper = puts "   type STOP to quit"
+    
+    puts ""
+    break puts "seassion has been Stop" if name == "STOP"
+end
+
+
+puts ""
+
+#4
+arr2 = [6, 3, 1, 8, 4, 2, 10, 65, 102]
+arr2.each do |number|
+    puts number if number % 2 == 0;
 end
